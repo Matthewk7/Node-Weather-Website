f03a7c1736a0d92c3601c3579eefce17053fcacf
Matthewk7 <matthewjkale@gmail.com>
2021-07-13 00:21:03 -0700

    Added OpenAPI Docs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
+const { kMaxLength } = require('buffer')
 
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const swaggerOptions={
+    definition: {
+        openapi:'3.0.0',
+        info: {
+            title: 'Weather API',
+            version: '1.0.0',
+            description:'Weather API to check the weather',
+            contact: {
+                name: "Matthew Kale",
+                url: "codingscape.com",
+                email: "Matthewjkale@gmail.com"
+            },
+            servers:[port]
+        }
+    },
+    apis: [__filename]
+}
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -21,13 +45,32 @@ hbs.registerPartials(partialsPath)
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Returns the homepage
+ *     responses:
+ *       200:
+ *         description: Weather
+ */
+app.get('/', (req, res) => {
     res.render('index', {
         title: 'Weather',
         name: 'Matthew Kale'
     })
 })
 
+/**
+ * @swagger
+ * /about:
+ *   get:
+ *     description: Returns the about page
+ *     responses:
+ *       200:
+ *         description: about
+ */
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
@@ -35,7 +78,15 @@ app.get('/about', (req, res) => {
     })
 })
 
-
+/**
+ * @swagger
+ * /help:
+ *   get:
+ *     description: Returns the help page
+ *     responses:
+ *       200:
+ *         description: help
+ */
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
@@ -44,6 +95,15 @@ app.get('/help', (req, res) => {
     })
 })
 
+/**
+ * @swagger
+ * /weather:
+ *   get:
+ *     description: Returns the weather
+ *     responses:
+ *       200:
+ *         description: weather
+ */
 app.get('/weather', (req, res) => {
     if(!req.query.address){
         return res.send({
@@ -71,17 +131,17 @@ app.get('/weather', (req, res) => {
     
 })
 
-app.get('/products', (req, res) => {
-    if(!req.query.search){
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
+// app.get('/products', (req, res) => {
+//     if(!req.query.search){
+//         return res.send({
+//             error: 'You must provide a search term'
+//         })
+//     }
+//     console.log(req.query.search)
+//     res.send({
+//         products: []
+//     })
+// })
 
 app.get('/help/*', (req, res) => {
     res.status(404).render('404', {
